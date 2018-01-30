06371f6be716c41170e43bf8611f463c89760eb5
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-01-30 13:49:22 +0000

    remove garbage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,22 +3,7 @@ import logo from '../../logo.svg';
 import './App.css';
 
 export default class App extends Component {
-  constructor(){
-    super()
-    this.flag = true
-    this.state ={}
-  }
-  changeState = () =>{
-    if(!this.flag){
-      return
-    }
-    this.flag = false
 
-    this.setState({
-      jose: 'jose'
-    })
-    console.log('asdadad')
-  }
   render() {
     console.log(this.state.jose)
     return (
@@ -27,7 +12,7 @@ export default class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro" onClick={this.changeState}>
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
